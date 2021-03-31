2ba0237d368971994aeb1ca75dd71767a62c561e
Liam <42282226+ImFascinated@users.noreply.github.com>
2021-03-31 18:08:19 +0100

    Added music inactivity

--- src/Features/Music.ts
+++ src/Features/Music.ts
@@ -2,6 +2,7 @@ import BatClient from '../../../Bat Bot/Bat Framework/dist/Client/BatClient';
 import FeatureBase from '../../../Bat Bot/Bat Framework/dist/Feature/FeatureBase';
 import { Manager, Player, Track, Node, Payload } from "erela.js";
 import Spotify from 'erela.js-spotify';
+import { GuildMember, VoiceState } from 'discord.js';
 
 const Config = require('../../config.json');
 
@@ -24,18 +25,23 @@ module.exports = class MusicFeature extends FeatureBase {
 				const guild = client.guilds.cache.get(id);
 				if (guild) guild.shard.send(payload);
 			},
-		})
+		});
+
 		client.manager.on("nodeConnect", (node: Node) => {
 			console.log(`Shiroko > Music node ${node.options.identifier} connected`)
-		})
+		});
+
 		client.manager.on("nodeError", (node: Node, error: Error) => {
 			console.log(`Shiroko > Music node ${node.options.identifier} had an error: ${error.message}`)
-		})
+		});
+
 		client.manager.on("trackStart", (player: Player, track: Track) => {
+			const requester: any = track.requester;
 			client.channels.cache
 				.get(player.textChannel)
-				.send(`**Playing** :notes: \`${track.title}\` - Now!`);
-		})
+				.send(`**Playing** :notes: \`${track.title}\` - Now!\nRequested by \`${requester.username}#${requester.discriminator}\``);
+		});
+
 		client.manager.on("queueEnd", (player: Player) => {
 			setTimeout(() => {
 				if (player.playing == false) {
@@ -46,10 +52,34 @@ module.exports = class MusicFeature extends FeatureBase {
 				}
 			}, 30000)
 		});
+
 		client.on("raw", (d: any) => client.manager.updateVoiceState(d));
 
 		client.once("ready", () => {
 			client.manager.init(client.user.id);
+		});
+
+		client.on('voiceStateUpdate', async (oldState: VoiceState, newState: VoiceState) => {
+			if (oldState == null) return;
+			if (oldState.channel == null) return;
+			const manager: Manager = await client.manager;
+
+			// Honestly.. find a better way to do this
+			const player = manager.players.get(newState.guild.id);
+			if (player == undefined) return;
+			if (player.voiceChannel == oldState.channelID) {
+				if (oldState.channel.members.size == 1) {
+					setTimeout(() => {
+						if (oldState.channel == null) return;
+						if (oldState.channel.members.size == 1) {
+							console.log(5)
+							const channel = client.channels.cache.get(player.textChannel);
+							channel.send(`Leaving due to inactivity`);
+							player.destroy();
+						}
+					}, 30000); // 30 Seconds
+				}
+			}
 		});
 	}
 }
